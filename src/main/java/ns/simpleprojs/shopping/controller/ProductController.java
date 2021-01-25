e9d7889f3b94aac8f52ce4ca58510f38e37fdccb
Nuno Silva <nunosilvacmj@hotmail.com>
2021-01-25 16:27:33 +0000

    add product controller get all mapping

--- src/main/java/ns/simpleprojs/shopping/controller/ProductController.java
+++ src/main/java/ns/simpleprojs/shopping/controller/ProductController.java
@@ -1,4 +1,23 @@
 package ns.simpleprojs.shopping.controller;
 
+import ns.simpleprojs.shopping.dao.ProductDao;
+import ns.simpleprojs.shopping.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    @Autowired
+    private ProductDao productDao;
+
+    @GetMapping({"/", ""})
+    public List<Product> getProducts() {
+        return productDao.findAll();
+    }
 }
